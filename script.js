b11970edc206519ef5e14e09f99a14ec85226a23
Himanshu Tiwari <himanshuhkt004@gmail.com>
2025-08-08 19:24:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,40 +13,51 @@ const changeturn=()=>{
   return turn === "X"?"0" : "X"
 }
 
-const checkwin=() =>{
-    let boxtext = document.getElementsByClassName('boxtext')
+const checkwin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
     let win = [
-    [0,1,2,0,5,0],
-    [3,4,5,0,15,0],
-    [6,7,8,0,25,0],
-    [0,3,6,-10,15,90],
-    [1,4,7,0,15,90],
-    [2,5,8,10,15,90],
-    [0,4,8,0,15,45],
-    [2,4,6,0,15,135]
-    ]
+        [0, 1, 2, 0, 0, 0],   // row 1
+        [3, 4, 5, 0, 1, 0],   // row 2
+        [6, 7, 8, 0, 2, 0],   // row 3
+        [0, 3, 6, -1, 1, 90], // col 1
+        [1, 4, 7, 0, 1, 90],  // col 2
+        [2, 5, 8, 1, 1, 90],  // col 3
+        [0, 4, 8, 0, 1, 45],  // diag 1
+        [2, 4, 6, 0, 1, 135]  // diag 2
+    ];
 
-    win.forEach(e=>{
-        if(boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
-           boxtext[e[1]].innerText === boxtext[e[2]].innerText &&
-           boxtext[e[0]].innerText !== '' ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " WON"
-            isgameover= true
+    // Get grid size and cell size
+    let container = document.querySelector('.container');
+    let cellSize = container.querySelector('.box').offsetWidth;
+
+    win.forEach(e => {
+        if (
+            boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[1]].innerText === boxtext[e[2]].innerText &&
+            boxtext[e[0]].innerText !== ''
+        ) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " WON";
+            isgameover = true;
 
             document.getElementById("winGif").style.display = "block";
-            document.querySelector('.horrorheadingcontainer').style.display = "flex"
-            cheering.loop = true
-            cheering.play()
-            crackers.loop = true
-            crackers.play()
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-            document.querySelector(".line").style.width = "30vw";
-            document.querySelector(".line").style.border = "2px solid black";
+            document.querySelector('.horrorheadingcontainer').style.display = "flex";
+            cheering.loop = true;
+            cheering.play();
+            crackers.loop = true;
+            crackers.play();
 
+            let line = document.querySelector(".line");
+            let translateX = e[3] * cellSize + cellSize / 2;
+            let translateY = e[4] * cellSize + cellSize / 2;
+            let angle = e[5];
+
+            line.style.width = `${cellSize * 3}px`; // full row/col/diag width
+            line.style.transform = `translate(${translateX}px, ${translateY}px) rotate(${angle}deg)`;
+            line.style.border = "2px solid black";
         }
-    })
+    });
+};
 
-}
 
 /*music.play();*/
 let boxes = document.getElementsByClassName("box")
